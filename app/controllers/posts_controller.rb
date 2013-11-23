d9847dc8272c183140847ff8bcf1874a0ccc9e74
Craig Chambers <craig__chambers@hotmail>
2013-11-23 16:42:34 +0900

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,74 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :vote]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_creator, only: [:edit, :update]
+
+
+  def index
+    @posts = Post.all.sort_by{|x| x.total_votes}.reverse
+  end
+
+  def show  
+  @comment = Comment.new  
+  end
+
+  def new
+    @post = Post.new
+
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.creator = current_user 
+
+    if @post.save
+      flash[:notice] = "Post was created."
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit    
+  end
+
+  def update  
+    if @post.update(post_params)
+      flash[:notice] = "The Post has been updated."
+       redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def vote
+    @vote = Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
+
+    respond_to do |format|
+      format.html do
+    if @vote.valid?
+      flash[:notice] = "Your vote has been counted."
+    else
+      flash[:error] = "You've already voted."
+    end
+    redirect_to :back
+    end
+    format.js
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description, category_ids: [])
+  end
+
+  def set_post
+    @post = Post.find_by slug: (params[:id])
+  end
+
+  def require_creator
+    access_denied unless logged_in? and (current_user == @post.creator || current_user.admin?)
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ActiveRecord::Base
+  include Sluggable
+
+	has_many :posts
+	has_many :comments
+	has_many :votes
+
+	has_secure_password validations: false
+
+	validates :username, presence: true, uniqueness: true
+	validates :password, presence: true, on: :create, length: {minimum: 3}
+
+	sluggable_column :username
+
+	def admin?
+	self.role == 'admin'
+	end
+
+
+end
